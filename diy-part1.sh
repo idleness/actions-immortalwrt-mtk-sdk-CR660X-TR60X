05fa6dff40a5cfaed07df29595102ff14d709aa5
liq <64891712+wosli@users.noreply.github.com>
2022-10-25 19:21:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/Dawneng/luci-theme-atmaterial.git package/luci-theme-atmaterial
+#git clone https://github.com/Dawneng/luci-theme-atmaterial.git package/luci-theme-atmaterial
 
